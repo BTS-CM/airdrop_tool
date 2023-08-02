c1ae06bfdf0b91819f890346abbea253cdd54936
CM <grctestemail@gmail.com>
2023-08-02 16:42:51 +0100

    Update getMarketParticipants.js

--- src/scripts/getMarketParticipants.js
+++ src/scripts/getMarketParticipants.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const fs = require('fs');
 const { humanReadableFloat } = require('./lib/common');
 
@@ -51,10 +52,28 @@ const getOrderBookData = async (baseSymbol, quoteSymbol, limit) => {
   };
 };
 
-const main = async () => {
+/**
+ * process orders, return airdrop data
+ * @param {Array} orders
+ * @param {Number} basePrecision
+ * @param {Float} avgPrice
+ * @returns {Array}
+ */
+const processOrders = (orders, basePrecision, avgPrice) => orders.map((order) => ({
+  value: 1 / (
+    Math.abs(
+      parseFloat(parseFloat(order.price).toFixed(basePrecision)) - parseFloat(parseFloat(avgPrice).toFixed(basePrecision))
+    ) + humanReadableFloat(1, basePrecision)
+  ),
+  id: order.owner_id,
+  name: order.owner_name,
+  qty: 1
+}));
+
+const getMarketParticipants = async () => {
   let orderBookData;
   try {
-    orderBookData = await getOrderBookData("BTS", "USD", 100);
+    orderBookData = await getOrderBookData("BTS", "XBTSX.USDT", 100);
   } catch (error) {
     console.log(error);
     return;
@@ -75,20 +94,45 @@ const main = async () => {
     const calculatedCer = quoteCerValue / baseCerValue; // USD/BTS == 20.79002079
 
     const mergedAsksBids = [...orderBook.bids, ...orderBook.asks];
-    const mappedOrders = mergedAsksBids.map((order) => ({
-      value: 1 / Math.abs(
-        parseFloat(parseFloat(order.price).toFixed(base.precision)) - calculatedCer
-      ),
-      id: order.owner_id,
-      name: order.owner_name,
-      qty: 1
-    }));
+    const mappedOrders = processOrders(mergedAsksBids, base.precision, calculatedCer);
 
     writeToFile(
-      './airdrops/marketParticipants.json',
+      './airdrops/smartcoinMarketParticipants.json',
       mappedOrders.sort((a, b) => b.value - a.value)
     );
+    return;
   }
+
+  // Non smartcoin - use top bid & lowest ask instead of cer
+  const topBid = orderBook.bids[0];
+  const lowestAsk = orderBook.asks[0];
+
+  const avgPrice = (parseFloat(topBid.price) + parseFloat(lowestAsk.price)) / 2;
+
+  const parsedBids = processOrders(orderBook.bids, base.precision, avgPrice);
+  const parsedAsks = processOrders(orderBook.asks, base.precision, avgPrice);
+
+  const mergedOrders = [...parsedBids, ...parsedAsks];
+  const finalResultValues = [];
+
+  // iterate through mergedOrders and tally the sum of values for each unique user id
+  mergedOrders.forEach((order) => {
+    const existingUserIndex = finalResultValues.findIndex((user) => user.id === order.id);
+    if (existingUserIndex !== -1) {
+      const newValue = { ...finalResultValues[existingUserIndex] };
+      newValue.value += order.value;
+      finalResultValues[existingUserIndex] = newValue;
+    } else {
+      finalResultValues.push(order);
+    }
+  });
+
+  console.log({ bids: parsedBids.find((acc) => acc.id === "1.2.1799499"), asks: parsedAsks.find((acc) => acc.id === "1.2.1799499") });
+
+  writeToFile(
+    './airdrops/marketParticipants.json',
+    finalResultValues.sort((a, b) => b.value - a.value)
+  );
 };
 
-main();
+getMarketParticipants();
